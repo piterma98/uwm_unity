f8c43fa401556deadce5cf2a73c68f2b5b1ccfdc
piterma98 <47064946+piterma98@users.noreply.github.com>
2021-10-27 20:22:58 +0200

    lab4

---
+++ lab_04/LookAround.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAround : MonoBehaviour
+{
+    // ruch wokół osi Y będzie wykonywany na obiekcie gracza, więc
+    // potrzebna nam referencja do niego (konkretnie jego komponentu Transform)
+    public Transform player;
+
+    public float sensitivity = 200f;
+
+    public float minAngle = -90.0f;
+    public float maxAngle = 90.0f;
+
+    private float mouseXMove = 0.0f;
+    private float mouseYMove = 0.0f;
+
+    void Start()
+    {
+        // zablokowanie kursora na środku ekranu, oraz ukrycie kursora
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mouseXMove += sensitivity * Time.deltaTime * Input.GetAxis("Mouse X");
+        mouseYMove -= sensitivity * Time.deltaTime * Input.GetAxis("Mouse Y");
+
+        mouseYMove = Mathf.Clamp(mouseYMove, minAngle, maxAngle);
+
+        transform.eulerAngles = new Vector3(mouseYMove, mouseXMove, 0.0f);
+    }
+}
---
+++ lab_04/Zad1.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class Zad1 : MonoBehaviour
+{
+    List<Vector3> positions = new List<Vector3>();
+    public float delay = 3.0f;
+    private int objectCounter = 0;
+    [SerializeField]
+    public int objectNumber = 5;
+    // obiekt do generowania
+    public GameObject block;
+    public Material[] materials = new Material[5];
+
+    void Start()
+    {
+        MeshRenderer meshRenderer = GetComponent<MeshRenderer>();
+        var gameobjectsize = meshRenderer.bounds;
+        while (positions.Count <= objectNumber)
+        {
+            Vector3 randomposition = new Vector3(Random.Range(gameobjectsize.min.x, gameobjectsize.max.x), 1, Random.Range(gameobjectsize.min.z, gameobjectsize.max.z));
+            if (!positions.Contains(randomposition)) {
+                this.positions.Add(randomposition);
+            }
+        }
+        // uruchamiamy coroutine
+        StartCoroutine(GenerujObiekt());
+    }
+
+    void Update()
+    {
+
+    }
+
+    IEnumerator GenerujObiekt()
+    {
+        Debug.Log("wywołano coroutine");
+        foreach (Vector3 pos in positions)
+        {
+            var gameobject = Instantiate(this.block, this.positions.ElementAt(this.objectCounter++), Quaternion.identity);
+            gameobject.GetComponent<MeshRenderer>().material = this.materials[Random.Range(0, this.materials.Length)];
+            yield return new WaitForSeconds(this.delay);
+        }
+        // zatrzymujemy coroutine
+        StopCoroutine(GenerujObiekt());
+    }
+}
